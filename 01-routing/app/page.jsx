9ea60fce3f5b5281dd5b8f74639b16b3561f43d9
Erhan <erhantezer68@gmail.com>
2023-01-24 03:11:37 +0300

    updated routing

--- 01-routing/app/page.jsx
+++ 01-routing/app/page.jsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
 
 
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Link href="/hakkimizda"> Hakkimizda</Link>
+      <br />
+      <Link
+      href={{
+      pathname:"/hakkimizda",
+      query: {
+        name: "Next.js",
+        surname: "ffff"},
+        }}
+      
+      >
+        hakkımızda obje
+      </Link>
+    </div>
   )
 }
